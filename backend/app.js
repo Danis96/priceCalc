dbf6a5f6facb9c16a947b75a9601c26144ec6c22
Danis96 <danis.preldzic@gmail.com>
2020-08-24 08:28:59 +0200

    backendRefactor

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const MongoClient = require("mongodb");
-const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const app = express();
-require('dotenv').config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 
-/// getting the schemas
-const User = require('./models/userInput');
-const Questions = require('./models/questions');
-
+/// importing routes for posting and getting
+/// questions
+const questionRoutes = require('./routes/question');
+/// users
+const userRoutes = require('./routes/user');
+/// email
+const emailRoutes = require('./routes/email');
 
 
 /// connection to the database
@@ -48,80 +48,8 @@ app.use((req, res, next) => {
   next();
 });
 
-/// post in db
-app.post('/api/user', (req, res, next) => {
-     const userAnswer = new User({
-       answers: req.body.answer,
-       time: req.body.estimatedTime,
-       price: req.body.estimatedPrice,
-       name: req.body.usersName,
-       email: req.body.usersEmail
-     });
-     console.log(userAnswer);
-     userAnswer.save();
-     res.status(201).json({
-          message: 'Users answers added succesfully'
-     });
-});
-
-
-app.get("/api/JSON", (req, res, next) => {
-  Questions.find().then(questions => {
-      console.log(questions.toString());
-      res.status(200).json({
-          message: "Questions fetched succesfully",
-          json: questions,
-      });
-  });
-});
-
-app.post("/sendmail", (req, res) => {
-    console.log("request came");
-    let user = req.body;
-    sendMail(user, info => {
-        res.status(201).json({
-            message: 'Email sent succesfully'
-        });
-    }).then(() => {
-        console.log(`The mail has been sent and the id is ${info.messageId}`);
-    });
-});
-
-async function sendMail(user, callback) {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
-    });
-
-    /// ovdje ce biti email od kompanije
-    const companyEmail = 'dzefkadzefka@gmail.com';
-
-    let mailOptions = {
-        from: '"Tech387 - App Estimation"<technodemailer@gmail.com>', // sender address
-        to:  user.usersEmail, companyEmail, // list of receivers
-        subject: "Regards from Tech387 Team - Here is your app estimation", // Subject line
-        html: `<h1>Hi ${user.usersName}</h1><br>
-                <p>We are very happy for contacting you.</p>
-                <p>We want to send you your app estimation.</p> <hr>
-                <p>Estimated time for your app was ${user.estimatedTime} hours</p>
-          <p>Your estimated app price was $ ${user.estimatedPrice}</p> <br><br><br><br>
-    <h4>We can't wait to hear from you again. Cheers!!!</h4>`
-    };
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail(mailOptions,(err, data) => {
-        if(err){
-            console.log('Error!!', err)
-        }else{
-            console.log('Email sent!')
-        }
-    });
-
-    callback(info);
-}
+app.use("/api/JSON", questionRoutes);
+app.use('/api/user', userRoutes);
+app.use('/sendmail', emailRoutes);
 
 module.exports = app;
---
+++ backend/routes/email.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+require('dotenv').config();
+
+
+const routerEmail = express.Router();
+
+
+routerEmail.post("", (req, res) => {
+    console.log("request came");
+    let user = req.body;
+    sendMail(user, info => {
+        res.status(201).json({
+            message: 'Email sent succesfully'
+        });
+    }).then(() => {
+        console.log(`The mail has been sent and the id is ${info.messageId}`);
+    });
+});
+
+async function sendMail(user, callback) {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    /// ovdje ce biti email od kompanije
+    const companyEmail = 'dzefkadzefka@gmail.com';
+
+    let mailOptions = {
+        from: '"Tech387 - App Estimation"<technodemailer@gmail.com>', // sender address
+        to:  user.usersEmail, companyEmail, // list of receivers
+        subject: "Regards from Tech387 Team - Here is your app estimation", // Subject line
+        html: `<h1>Hi ${user.usersName}</h1><br>
+                <p>We are very happy for contacting you.</p>
+                <p>We want to send you your app estimation.</p> <hr>
+                <p>Estimated time for your app was ${user.estimatedTime} hours</p>
+          <p>Your estimated app price was $ ${user.estimatedPrice}</p> <br><br><br><br>
+    <h4>We can't wait to hear from you again. Cheers!!!</h4>`
+    };
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions,(err, data) => {
+        if(err){
+            console.log('Error!!', err)
+        }else{
+            console.log('Email sent!')
+        }
+    });
+
+    callback(info);
+}
+
+
+
+module.exports = routerEmail;
