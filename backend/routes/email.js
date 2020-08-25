823f7b82f0775c0a982f45a0796d9830e2a5fb9a
Elvir9 <ehalilovicc@gmail.com>
2020-08-25 17:13:02 +0200

    email

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -22,10 +22,10 @@ routerEmail.post("", (req, res) => {
 async function sendMail(user, callback) {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        // service:'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        service: 'Gmail',
+        // host: 'smtp.gmail.com',
+        // port: 465,
+        // secure: true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASSWORD
@@ -34,7 +34,7 @@ async function sendMail(user, callback) {
 
     let mailOptions = {
         from: '"Tech387 - App Estimation"<technodemailer@gmail.com>', // sender address
-        to:  ` ${user.usersEmail}, support@tech387.com `, // list of receivers
+        to: ` ${user.usersEmail}, support@tech387.com `, // list of receivers
         subject: "Regards from Tech387 Team - Here is your app estimation", // Subject line
         html: `<h1>Hi ${user.usersName}</h1><br>
                 <p>We are very happy for contacting you.</p>
@@ -45,10 +45,10 @@ async function sendMail(user, callback) {
     };
 
     // send mail with defined transport object
-    let info = await transporter.sendMail(mailOptions,(err, data) => {
-        if(err){
+    let info = await transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
             console.log('Error!!', err)
-        }else{
+        } else {
             console.log('Email sent!')
         }
     });
