015f942cf967cdfd6abe2cfa72c4037b1aa9d024
Danis96 <danis.preldzic@gmail.com>
2020-08-26 19:20:28 +0200

    coefficient

--- src/app/estimate/estimate.service.ts
+++ src/app/estimate/estimate.service.ts
@@ -8,6 +8,7 @@ export class EstimateService {
 
 
     infoFetched;
+    finalCoefficient = 0;
 
   estimatedTimeAndPrice: EstimatedTimeAndPrice = {
     price: 0,
@@ -21,17 +22,34 @@ export class EstimateService {
      return  this.subject.asObservable();
   }
 
-    sendEstimatedTimeAndPrice(event, price: number, time: number, radio: boolean, num: number, pageID: number, pages:any, i:number) {
+    sendEstimatedTimeAndPrice(event, price: number, time: number, radio: boolean, num: number, pageID: number, pages:any, i:number, coefficient: number) {
         if(event.target.checked) {
-          this.estimatedTimeAndPrice.price += price;
-          this.estimatedTimeAndPrice.time += time;
-          console.log('Price: ' + this.estimatedTimeAndPrice.price.toString());
-          console.log('Time: ' + this.estimatedTimeAndPrice.time.toString());
+            if(coefficient) {
+                 this.finalCoefficient += coefficient;
+                 console.log(this.finalCoefficient);
+            }
+            if(!coefficient) {
+                let priceCoefficient = this.finalCoefficient * price;
+                let timeCoefficient = this.finalCoefficient * time;
+                console.log(this.finalCoefficient);
+                this.estimatedTimeAndPrice.price += priceCoefficient;
+                this.estimatedTimeAndPrice.time += timeCoefficient;
+                console.log('Price: ' + this.estimatedTimeAndPrice.price.toString());
+                console.log('Time: ' + this.estimatedTimeAndPrice.time.toString());
+            }
         }  else {
-          this.estimatedTimeAndPrice.price -= price;
-          this.estimatedTimeAndPrice.time  -= time;
-          console.log('Price: ' + this.estimatedTimeAndPrice.price.toString());
-          console.log('Time: ' + this.estimatedTimeAndPrice.time.toString());
+            if(coefficient) {
+                this.finalCoefficient -= coefficient;
+                console.log(this.finalCoefficient);
+            }
+            if(!coefficient) {
+                let priceCoefficient = this.finalCoefficient * price;
+                let timeCoefficient = this.finalCoefficient * time;
+                this.estimatedTimeAndPrice.price -= priceCoefficient;
+                this.estimatedTimeAndPrice.time  -= timeCoefficient;
+                console.log('Price: ' + this.estimatedTimeAndPrice.price.toString());
+                console.log('Time: ' + this.estimatedTimeAndPrice.time.toString());
+            }
         }
         this.subject.next({
           price: this.estimatedTimeAndPrice.price,
@@ -39,4 +57,6 @@ export class EstimateService {
         });
   }
 }
+
+
 
