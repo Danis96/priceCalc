3e9992a837ddc738a3ded872879ea9fe9b7c8567
Danis96 <danis.preldzic@gmail.com>
2020-08-27 09:23:41 +0200

    fix

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -42,13 +42,19 @@ async function sendMail(user, callback) {
         to: `support@tech387.com , ${user.usersEmail}`, // list of receivers
         subject: "Regards from Tech387 Team - Here is your app estimation", // Subject line
         html: `<h1>Hi ${user.usersName}</h1><br>
-                <p>We are very happy for contacting you.</p>
-                <p>We want to send you your app estimation.</p> <hr>
-                <p>Estimated time for your app was ${user.estimatedTime} hours</p>
-          <p>Your estimated app price was $ ${user.estimatedPrice}</p> <br>
+                <p>This email is to let you know your bill estimate.</p>
+                <p>Each project is a little bit different. Our software cost calculator can help you get a ballpark figure.
+                 Tech 387 will call you to fine tune your quote so that you are not payinh for things you don't want or need.</p> <br>
+                <p>You don't need to take any action, this is just an estimate.</p>
+                <p style="font-size: 20px">Estimated project time</p><p style="font-size: 22px; font-weight: bold"> ${user.estimatedTime} hours.</p> <br>
+                <p style="font-size: 20px">Estimated project price</p><p style="font-size: 22px; font-weight: bold">USD ${user.estimatedPrice}.</p> <br> <br>
+                <p>Project features: </p>
           <p>${userQuestions}</p>
           <br><br><br>
-    <h4>We can't wait to hear from you again. Cheers!!!</h4>`
+          <p>Please contact us with any questions, we are always happy to help.</p><br>
+          <p>Cheers, </p>
+          <p>Tech 387</p>`
+          
     };
 
     // send mail with defined transport object
